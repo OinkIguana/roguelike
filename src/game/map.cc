9613a47f394acabbc52fd89f2acf63b910fa5e96
Cameron Eldridge <cameldridge+git@gmail.com>
2017-04-24 17:46:13 -0400

    Remove dead ends and double hallways

--- src/game/map.cc
+++ src/game/map.cc
@@ -29,15 +29,138 @@ void flood_fill(std::vector<std::vector<T>>& grid, unsigned int x, unsigned int 
     }
 }
 
+
 const int MIN_ROOMS = 5;
 const int MAX_ROOMS = 30;
 const float GROWTH_FACTOR = 1.5f;
 
+struct Rectangle { int x, y, w, h; };
+struct Point { int x, y; };
+
 namespace Game {
+    bool is_dead_end(std::vector<std::vector<std::shared_ptr<Cell>>>& cells, unsigned int x, unsigned int y) {
+        if(cells[y][x]->type != Cell::Type::Hall && cells[y][x]->type != Cell::Type::Door) return false;
+        int count = 0;
+        if(y == 0 || (cells[y-1][x]->type != Cell::Type::Hall && cells[y-1][x]->type != Cell::Type::Door && cells[y-1][x]->type != Cell::Type::Room)) {
+            ++count;
+        }
+        if(x == 0 || (cells[y][x-1]->type != Cell::Type::Hall && cells[y][x-1]->type != Cell::Type::Door && cells[y][x-1]->type != Cell::Type::Room)) {
+            ++count;
+        }
+        if(y == cells.size() - 1 || (cells[y+1][x]->type != Cell::Type::Hall && cells[y+1][x]->type != Cell::Type::Door && cells[y+1][x]->type != Cell::Type::Room)) {
+            ++count;
+        }
+        if(x == cells[0].size() - 1 || (cells[y][x+1]->type != Cell::Type::Hall && cells[y][x+1]->type != Cell::Type::Door && cells[y][x+1]->type != Cell::Type::Room)) {
+            ++count;
+        }
+        return count == 3;
+    }
+
+    bool is_fat_hallway(std::vector<std::vector<std::shared_ptr<Cell>>>& cells, unsigned int x, unsigned int y) {
+        if(cells[y][x]->type != Cell::Type::Hall && cells[y][x]->type != Cell::Type::Door) return false;
+        #define IS_A_ROOM(a) (a->type == Cell::Type::Hall || a->type == Cell::Type::Door || a->type == Cell::Type::Room)
+        // this needs work
+        Point sides[] = {
+            { -1, -1 },
+            { -1,  0 },
+            { -1,  1 },
+            {  0,  1 },
+            {  1,  1 },
+            {  1,  0 },
+            {  1, -1 },
+            {  0, -1 }
+        };
+        int start = 0;
+        while(
+            y + sides[start].y >= 0 &&
+            y + sides[start].y < cells.size() &&
+            x + sides[start].x >= 0 &&
+            x + sides[start].x < cells[0].size() &&
+            IS_A_ROOM(cells[y+sides[start].y][x+sides[start].x])
+        ) {
+            if(++start == 6) { return true; }
+        }
+        int cycle = 0;
+        bool four = false;
+        for(int i = 0; i < 8; ++i) {
+            Point side = sides[(i + start) % 8];
+            if(0 <= y + side.y && y + side.y < cells.size()) {
+                if(0 <= x + side.x && x + side.x < cells[0].size()) {
+                    if(IS_A_ROOM(cells[y+side.y][x+side.x])) {
+                        ++cycle;
+                        if(cycle == 6) {
+                            return true;
+                        } else if(cycle == 4) {
+                            four = true;
+                        }
+                        continue;
+                    }
+                }
+            }
+            if(cycle && cycle < 4 && four) {
+                return false;
+            } else {
+                cycle = 0;
+            }
+        }
+        return four && (cycle == 0 || cycle >= 4);
+        #undef IS_A_ROOM
+    }
+
+    void remove_dead_end(std::vector<std::vector<std::shared_ptr<Cell>>>& cells, unsigned int x, unsigned int y) {
+        if(is_dead_end(cells, x, y)) {
+            if(cells[y][x]->type == Cell::Type::Hall) {
+                cells[y][x]->type = Cell::Type::Empty;
+                if(y != 0) {
+                    remove_dead_end(cells, x, y - 1);
+                }
+                if(x != 0) {
+                    remove_dead_end(cells, x - 1, y);
+                }
+                if(y != cells.size() - 1) {
+                    remove_dead_end(cells, x, y + 1);
+                }
+                if(x != cells[0].size() - 1) {
+                    remove_dead_end(cells, x + 1, y);
+                }
+            } else if(cells[y][x]->type == Cell::Type::Door) {
+                if(y == 0 || y == cells.size() -1 || cells[y + 1][x]->type == Cell::Type::Room || cells[y - 1][x]->type == Cell::Type::Room) {
+                    cells[y][x]->type = Cell::Type::WallH;
+                } else if(x == 0 || x == cells[0].size() - 1 || cells[y][x + 1]->type == Cell::Type::Room || cells[y][x - 1]->type == Cell::Type::Room) {
+                    cells[y][x]->type = Cell::Type::WallV;
+                }
+            }
+        }
+    }
+
+    void remove_fat_hallways(std::vector<std::vector<std::shared_ptr<Cell>>>& cells, unsigned int x, unsigned int y) {
+        if(is_fat_hallway(cells, x, y)) {
+            if(cells[y][x]->type == Cell::Type::Hall) {
+                cells[y][x]->type = Cell::Type::Empty;
+            } else if(cells[y][x]->type == Cell::Type::Door) {
+                if(y == 0 || y == cells.size() - 1 || cells[y + 1][x]->type == Cell::Type::Room || cells[y - 1][x]->type == Cell::Type::Room) {
+                    cells[y][x]->type = Cell::Type::WallH;
+                } else if(x == 0 || x == cells[0].size() - 1 || cells[y][x + 1]->type == Cell::Type::Room || cells[y][x - 1]->type == Cell::Type::Room) {
+                    cells[y][x]->type = Cell::Type::WallV;
+                }
+            }
+            if(y > 0) {
+                remove_fat_hallways(cells, x, y - 1);
+            }
+            if(x > 0) {
+                remove_fat_hallways(cells, x - 1, y);
+            }
+            if(y < cells.size() - 1) {
+                remove_fat_hallways(cells, x, y + 1);
+            }
+            if(x < cells[0].size() - 1) {
+                remove_fat_hallways(cells, x + 1, y);
+            }
+        }
+    }
+
     Map::Map(int level) {
         rng.seed(level);
-        struct Rectangle { int x, y, w, h; };
-        struct Point { int x, y; };
 
         const int rm_count = std::min(MIN_ROOMS + level / 3, MAX_ROOMS);
         const int max_merges = rm_count / 2;
@@ -64,23 +187,22 @@ namespace Game {
         auto rm_boxes = std::vector<Rectangle>();
         for(int i = 0; i < rm_count; ++i) {
             int xx, yy, ww, hh;
-            bool collides = false;
+            int collides = 0;
             do {
                 xx = rx(rng);
                 yy = ry(rng);
-                ww = ceil(rw(rng)) + 2;
-                hh = ceil(rh(rng)) + 2;
+                ww = std::ceil(rw(rng)) + 2;
+                hh = std::ceil(rh(rng)) + 2;
                 // only allow collisions sometimes
-                bool allow_merge = merges < max_merges && !rcoll(rng);
-                collides = any_of(rm_boxes.begin(), rm_boxes.end(), [&xx, &yy, &ww, &hh] (Rectangle rm) {
+                collides = std::count_if(rm_boxes.begin(), rm_boxes.end(), [&xx, &yy, &ww, &hh] (Rectangle rm) {
                     return
                         rm.x + rm.w >= xx && rm.x < xx + ww &&
                         rm.y + rm.h >= yy && rm.y < yy + hh;
                 });
+                bool allow_merge = merges + collides <= max_merges && !rcoll(rng);
                 bool out_of_bounds = xx + ww >= w || yy + hh > h;
                 if(collides && allow_merge && !out_of_bounds) {
-                    collides = false;
-                    ++merges;
+                    merges += collides;
                 }
                 collides = (collides && !allow_merge) || out_of_bounds;
             } while(collides);
@@ -91,13 +213,19 @@ namespace Game {
         for(auto box : rm_boxes) {
             for(int y = box.y; y < box.y + box.h; ++y) {
                 for(int x = box.x; x < box.x + box.w; ++x) {
-                    if(cells[y][x]->type != Cell::Type::Room && cells[y][x]->type != Cell::Type::Room) {
+                    if(cells[y][x]->type != Cell::Type::Room) {
                         if((x == box.x || x == box.x + box.w - 1) && (y == box.y || y == box.y + box.h - 1)) {
                             cells[y][x]->type = Cell::Type::Corner;
                         } else if(x == box.x || x == box.x + box.w - 1) {
-                            cells[y][x]->type = Cell::Type::WallV;
+                            if(cells[y][x]->type == Cell::Type::WallH || cells[y][x]->type == Cell::Type::Corner)
+                                cells[y][x]->type = Cell::Type::Corner;
+                            else
+                                cells[y][x]->type = Cell::Type::WallV;
                         } else if(y == box.y || y == box.y + box.h - 1) {
-                            cells[y][x]->type = Cell::Type::WallH;
+                            if(cells[y][x]->type == Cell::Type::WallV || cells[y][x]->type == Cell::Type::Corner)
+                                cells[y][x]->type = Cell::Type::Corner;
+                            else
+                                cells[y][x]->type = Cell::Type::WallH;
                         } else {
                             cells[y][x]->type = Cell::Type::Room;
                         }
@@ -129,16 +257,20 @@ namespace Game {
 
         // add hallways
         std::vector<int> connected(r);
-        std::vector<std::array<int, 4>> tunnels(r - 1, std::array<int, 4>{ 0, 0, 0, 0 });
+        for(int i = 0; i < r; ++i) { connected[i] = i + 1; }
+        std::vector<std::array<int, 4>> tunnels(r, std::array<int, 4>{ 0, 0, 0, 0 });
         std::uniform_int_distribution<int> rd(0, 3);
-        for(unsigned int i = 0; i < connected.size(); ++i) { connected[i] = i + 1; }
-        while(!all_of(connected.begin(), connected.end(), [](int is){ return is == 1; })) {
+        std::binomial_distribution<bool> rcont(rm_count / MAX_ROOMS / 4);
+        while(!all_of(connected.begin(), connected.end(), [](int is){ return is == 1; }) || rcont(rng)) {
             ++r;
             std::uniform_int_distribution<int> rr(0, rm_cells.size() - 1);
             int xx, yy, rm, dir;
             do {
+                // NOTE: if you're unlucky this will go on for a long time...
                 dir = rd(rng);
-                rm = rr(rng);
+                do {
+                    rm = rr(rng);
+                } while(rm_cells[rm].empty());
                 std::uniform_int_distribution<int> rc(0, rm_cells[rm].size() - 1);
                 int p = rc(rng);
                 xx = rm_cells[rm][p].x;
@@ -146,7 +278,7 @@ namespace Game {
             } while(!(
                 cl_graph[yy][xx] != 0 && // in a room
                 tunnels[cl_graph[yy][xx] - 1][dir] == 0 && // not already a tunnel in this direction
-                (cells[yy][xx]->type == Cell::Type::Room || cells[yy][xx]->type != Cell::Type::Hall) // actually in the room part
+                (cells[yy][xx]->type == Cell::Type::Room || cells[yy][xx]->type == Cell::Type::Hall) // actually in the room part
             ));
 
             tunnels.emplace_back(std::array<int, 4>{ !(dir % 2), dir % 2, !(dir % 2), dir % 2 });
@@ -155,21 +287,32 @@ namespace Game {
             connected.emplace_back(connected[rm]);
             ++tunnels[rm][dir];
             while(xx >= 0 && xx < w && yy >= 0 && yy < h) {
-                if(cells[yy][xx]->type == Cell::Type::Empty) {
+                switch(cells[yy][xx]->type) {
+                case Cell::Type::Empty:
                     cells[yy][xx]->type = Cell::Type::Hall;
                     cl_graph[yy][xx] = r;
                     rm_cells.back().emplace_back(Point{ xx, yy });
-                } else if(cells[yy][xx]->type == Cell::Type::WallH || cells[yy][xx]->type == Cell::Type::WallV) {
+                    break;
+                case Cell::Type::WallH:
+                case Cell::Type::WallV:
                     cells[yy][xx]->type = Cell::Type::Door;
                     rm_cells.back().emplace_back(Point{ xx, yy });
-                } else if((cells[yy][xx]->type == Cell::Type::Room || cells[yy][xx]->type == Cell::Type::Hall) && cl_graph[yy][xx] - 1 != rm) {
-                    int low = std::min(connected[cl_graph[yy][xx] - 1], connected[rm]);
-                    int hi = std::max(connected[cl_graph[yy][xx] - 1], connected[rm]);
-                    std::replace(connected.begin(), connected.end(), hi, low);
-                    ++tunnels[cl_graph[yy][xx] - 1][(dir + 2) % 4];
                     break;
-                } else if(cells[yy][xx]->type == Cell::Type::Corner) {
+                case Cell::Type::Hall:
+                    if(cl_graph[yy][xx] - 1 != rm) {
+                        rm_cells.back().emplace_back(Point{ xx, yy });
+                    }
+                case Cell::Type::Room:
+                    if(cl_graph[yy][xx] - 1 != rm) {
+                        int low = std::min(connected[cl_graph[yy][xx] - 1], connected[rm]);
+                        int hi = std::max(connected[cl_graph[yy][xx] - 1], connected[rm]);
+                        std::replace(connected.begin(), connected.end(), hi, low);
+                        ++tunnels[cl_graph[yy][xx] - 1][(dir + 2) % 4];
+                        goto quit;
+                    }
                     break;
+                default:
+                    goto quit;
                 }
                 switch(dir) {
                     case 0: xx += 1; break;
@@ -178,17 +321,75 @@ namespace Game {
                     case 3: yy -= 1; break;
                 }
             }
+        quit:;
         }
 
-        // remove dead ends
+        // remove dead ends/fat hallways
+        for(int y = 0; y < h; ++y) {
+            for(int x = 0; x < w; ++x) {
+                if(is_fat_hallway(cells, x, y)) {
+                    remove_fat_hallways(cells, x, y);
+                }
+            }
+        }
+        for(int y = 0; y < h; ++y) {
+            for(int x = 0; x < w; ++x) {
+                if(is_dead_end(cells, x, y)) {
+                    remove_dead_end(cells, x, y);
+                }
+            }
+        }
+
+        // add walls/corners to their rooms
+        for(int y = 0; y < h; ++y) {
+            for(int x = 0; x < w; ++x) {
+                switch(cells[y][x]->type) {
+                case Cell::Type::WallH:
+                    if(y != 0 && cells[y-1][x]->type == Cell::Type::Room) {
+                        rm_cells[cl_graph[y-1][x]-1].emplace_back(Point{ x, y });
+                    }
+                    if(y != h - 1 && cells[y+1][x]->type == Cell::Type::Room) {
+                        rm_cells[cl_graph[y+1][x]-1].emplace_back(Point{ x, y });
+                    }
+                    break;
+                case Cell::Type::WallV:
+                    if(x != 0 && cells[y][x-1]->type == Cell::Type::Room) {
+                        rm_cells[cl_graph[y][x-1]-1].emplace_back(Point{ x, y });
+                    }
+                    if(x != w - 1 && cells[y][x+1]->type == Cell::Type::Room) {
+                        rm_cells[cl_graph[y][x+1]-1].emplace_back(Point{ x, y });
+                    }
+                    break;
+                case Cell::Type::Corner:
+                    if(x != 0 && y != 0 && cells[y-1][x-1]->type == Cell::Type::Room) {
+                        rm_cells[cl_graph[y-1][x-1]-1].emplace_back(Point{ x, y });
+                    }
+                    if(x != 0 && y != h - 1 && cells[y+1][x-1]->type == Cell::Type::Room) {
+                        rm_cells[cl_graph[y+1][x-1]-1].emplace_back(Point{ x, y });
+                    }
+                    if(x != w - 1 && y != 0 && cells[y-1][x+1]->type == Cell::Type::Room) {
+                        rm_cells[cl_graph[y-1][x+1]-1].emplace_back(Point{ x, y });
+                    }
+                    if(x != w - 1 && y != h - 1 && cells[y+1][x+1]->type == Cell::Type::Room) {
+                        rm_cells[cl_graph[y+1][x+1]-1].emplace_back(Point{ x, y });
+                    }
+                    break;
+                default: break;
+                }
+            }
+        }
 
         // assign Cells to Rooms
-        rooms = std::vector<std::shared_ptr<Room>>(r);
-        std::transform(rm_cells.begin(), rm_cells.end(), rooms.begin(), [this] (std::vector<Point> points) {
-            std::vector<std::shared_ptr<Cell>> cls(points.size());
-            std::transform(points.begin(), points.end(), cls.begin(), [this] (Point point) { return cells[point.y][point.x]; });
-            return std::make_shared<Room>(cls);
-        });
+        rooms = std::vector<std::shared_ptr<Room>>();
+        for(auto points : rm_cells) {
+            std::vector<std::shared_ptr<Cell>> cls;
+            for(Point p : points) {
+                if(cells[p.y][p.x]->type != Cell::Type::Empty)
+                    cls.emplace_back(cells[p.y][p.x]);
+            }
+            if(cls.size() > 1)
+                rooms.emplace_back(std::make_shared<Room>(cls));
+        }
     }
 
     std::string Map::to_string() const {

0edd03608bad2aeea57eabd6f4dfb467dcec0a8f
Cameron Eldridge <cameldridge+git@gmail.com>
2017-08-19 13:17:23 -0400

    Almost working iterator based chain method

--- rust/src/actors/bat.rs
+++ rust/src/actors/bat.rs
@@ -1,5 +1,5 @@
 use rand::{thread_rng,Rng};
-use engine::{Actor,Action,Behavior,Direction,start};
+use engine::{Actor,Action,Behavior,Direction,start,perform};
 
 #[derive(Clone)]
 pub struct Bat;
@@ -8,7 +8,10 @@ impl Actor for Bat {
     fn react(&self, _: Action) -> Box<Behavior> {
         let mut dirs = Direction::cardinals();
         thread_rng().shuffle(&mut dirs);
-        start()
+        start().switch(
+            dirs.into_iter()
+                .map(|d| start().if_attackable(d, *perform(Action::Attack(d))))
+        )
     }
     fn can_be_attacked(&self) -> bool { true }
     fn symbol(&self) -> char { 'B' }
--- rust/src/engine/behavior/mod.rs
+++ rust/src/engine/behavior/mod.rs
@@ -1,41 +1,79 @@
 use std::mem::replace;
-use super::{Action,Map};
+use super::{Action,Map,Direction};
 
 pub struct Start;
 impl Behavior for Start {
-    fn exec(&self, _: usize, _: &mut Map) {}
+    fn exec(&self, _: usize, _: &mut Map) -> bool { false }
+}
+
+pub struct Switch<T: Behavior, I>(I) where I: Iterator<Item=Box<T>>;
+impl<T: Behavior, I> Behavior for Switch<T, I> where I: Iterator<Item=Box<T>> {
+    fn exec(&self, i: usize, map: &mut Map) -> bool {
+        for b in self.0 {
+            if b.exec(i, map) { return true; }
+        }
+        false
+    }
 }
 
 pub struct Perform(Action);
 impl Behavior for Perform {
-    fn exec(&self, i: usize, map: &mut Map) {
+    fn exec(&self, i: usize, map: &mut Map) -> bool {
         match self.0 {
             Action::Move(dir) => {
                 for neighbour in map.get_neighbouring_tile_index(i, dir) {
                     let (a, b) = map.tiles[i].clone().move_to(map.tiles[neighbour].clone());
                     replace(&mut map.tiles[i], a);
                     replace(&mut map.tiles[neighbour], b);
+                    return true;
                 }
+                return false;
             },
-            _ => ()
+            _ => false
         }
     }
 }
 
 pub struct Then<T: Behavior, U: Behavior>(T, U);
 impl<T: Behavior, U: Behavior> Behavior for Then<T, U> {
-    fn exec(&self, i: usize, map: &mut Map) {
-        &self.0.exec(i, map);
-        &self.1.exec(i, map);
+    fn exec(&self, i: usize, map: &mut Map) -> bool {
+        self.0.exec(i, map) || self.1.exec(i, map)
+    }
+}
+
+pub struct IfAttackable<T: Behavior>(Direction, T);
+impl<T: Behavior> Behavior for IfAttackable<T> {
+    fn exec(&self, i: usize, map: &mut Map) -> bool {
+        let attackable = map
+            .get_neighbouring_tile_index(i, self.0)
+            .map(|n| &map.tiles[n])
+            .and_then(|t| t.contents().clone())
+            .map(|a| a.can_be_attacked())
+            .unwrap_or(false);
+        if attackable {
+            return self.1.exec(i, map)
+        }
+        attackable
     }
 }
 
 pub trait Behavior {
-    fn then<U: Behavior>(self, next: U) -> Then<Self, U> where Self: Behavior + Sized {
-        Then(self, next)
+    fn then<U>(self, next: U) -> Box<Then<Self, U>>
+    where Self: Behavior + Sized, U: Behavior {
+        Box::new(Then(self, next))
     }
 
-    fn exec(&self, i: usize, &mut Map);
+    fn switch<T: Behavior, I>(self, iter: I) -> Box<Switch<T, I>>
+    where Self: Behavior + Sized, I: Iterator<Item=Box<T>> {
+        Box::new(Switch(iter))
+    }
+
+    fn if_attackable<T>(self, dir: Direction, b: T) -> Box<IfAttackable<T>>
+    where Self: Behavior + Sized, T: Behavior {
+        Box::new(IfAttackable(dir, b))
+    }
+
+    fn exec(&self, i: usize, &mut Map) -> bool;
 }
 
 pub fn start() -> Box<Start> {

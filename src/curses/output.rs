55c401b1e61ea692b83397b45d0e206919f89c24
Cameron Eldridge <cameldridge+git@gmail.com>
2017-08-19 15:20:00 -0400

    Add border around map in display

--- src/curses/output.rs
+++ src/curses/output.rs
@@ -13,11 +13,17 @@ impl<'a> Output<'a> {
 }
 impl<'a> Outputter for Output<'a> {
     fn render(&self, state: &State) {
+        // TODO: large map panning
         let map_str: String = state.map.tiles.iter().map(|ref tile| tile.symbol()).collect();
-        self.window.mv(0, 0);
         if state.map.width > 0 {
-            for row in map_str.as_bytes().chunks(state.map.width).map(|row| from_utf8(row).unwrap()) {
-                self.window.addstr(&(row.to_owned() + "\n"));
+            self.window.mvaddstr(0, 1, &String::from_utf8(vec![b'-'; state.map.width]).unwrap());
+            for (i, row) in map_str.as_bytes().chunks(state.map.width).map(|row| from_utf8(row).unwrap()).enumerate() {
+                self.window.mvaddstr(i as i32 + 1, 1, &(row.to_owned()));
+            }
+            self.window.mvaddstr(state.map.height as i32 + 1, 1, &String::from_utf8(vec![b'-'; state.map.width]).unwrap());
+            for i in 0..state.map.height as i32 + 2 {
+                self.window.mvaddstr(i, 0, "|");
+                self.window.mvaddstr(i, state.map.width as i32 + 1, "|");
             }
         }
         self.window.refresh();
